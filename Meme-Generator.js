afef6c0aac80ded8fcae9b70eaf95499eb531ced
U-HOME\Kyle <coda2@illinois.edu>
2016-10-21 21:04:15 -0500

    Updated syntax error

--- Meme-Generator.js
+++ Meme-Generator.js
@@ -1,5 +1,5 @@
 var noun = ["asshole", "dick", "retard", "Nick", "Gavin", "Ryze", "meme", "Cody", "Zac", "Steve", "level 5 Nunu", "fed Amumu", "fam", "Kyle", "David Becker", "Overwatch Brother", "Goblin", "Joey Salads", "Berger", "Donald Dump", "Sona time"];
-var adjective = ["savage", "retarded", "shitty", "GG", "fed", "OP", "naked", "Smelly Boy", "Stinky", "Vaper" "Dank", "Prime"];
+var adjective = ["savage", "retarded", "shitty", "GG", "fed", "OP", "naked", "Smelly Boy", "Stinky", "Vaper", "Dank", "Prime"];
 
 function randomize(){
     var random_noun = Math.floor(Math.random()*noun.length);
