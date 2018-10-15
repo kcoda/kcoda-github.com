04fcf5c6e2af013ee5b0e7749c561539146f2528
kcoda <kyle.coda@gmail.com>
2018-10-15 15:59:54 -0500

    Delete Meme-Generator.js

--- Meme-Generator.js
+++
@@ -1,9 +0,0 @@
-var noun = ["asshole", "dick", "retard", "Nick", "Gavin", "Ryze", "meme", "Cody", "Zac", "Steve", "level 5 Nunu", "fed Amumu", "fam", "Kyle", "David Becker", "Overwatch Brother", "Goblin", "Joey Salads", "Berger", "Donald Dump", "Sona time"];
-var adjective = ["savage", "retarded", "shitty", "GG", "fed", "OP", "naked", "Smelly Boy", "Stinky", "Vaper", "Dank", "Prime"];
-
-function randomize(){
-    var random_noun = Math.floor(Math.random()*noun.length);
-    var random_adjective = Math.floor(Math.random()*adjective.length);
-    var string = adjective[random_adjective] + " " + noun[random_noun];
-    document.getElementById("textbox").innerHTML = string;
-}
